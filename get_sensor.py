a22d40c3068ff82d264f64fcaa329271f7f1fb67
hsji123 <uren0927@gmail.com>
2015-09-16 01:31:37 +0900

    test1
    test

--- get_sensor.py
+++ get_sensor.py
@@ -3,8 +3,6 @@
 import serial,os,time
 import sys
 import MySQLdb
-import logging
-import logging.handlers
 import datetime
 
 print "test uart"
@@ -17,69 +15,69 @@ cursor = db.cursor()
 print "successfully connect"
 
 while 1:
-	data = ser.read(28)
-	print data
-	temp = 0
-	humi = 0
-	co2 = 0
-#	print data[2], data[3], data[4], data[6]
-#	print data[10], data[11], data[12], data[14]
-#	print data[23]
-	if not (data[2].isdigit() & data[3].isdigit() & data[4].isdigit() & data[6].isdigit()) :
-		print 3
-		continue
-	if not (data[10].isdigit() & data[11].isdigit() & data[12].isdigit() & data[14].isdigit()) :
-		print 4
-		continue
-	if not (data[23].isdigit()) :
-		print 5
-		continue
-#	print 2
-	if (data[1] is '+') :
-		temp += (int(data[2])) * 100
-		temp += (int(data[3])) * 10
-		temp += (int(data[4])) * 1
-		temp += (int(data[6])) * 0.1
-	else :
-		temp += (int(data[2])) * 100
-		temp += (int(data[3])) * 10
-		temp += (int(data[4])) * 1
-		temp += (int(data[6])) * 0.1
-		temp * -1
-	
-	if (data[9] is '+') :
-		humi += (int(data[10])) * 100
-		humi += (int(data[11])) * 10
-		humi += (int(data[12])) * 1
-		humi += (int(data[14])) * 0.1
-	else :
-		humi += (int(data[10])) * 100
-		humi += (int(data[11])) * 10
-		humi += (int(data[12])) * 1
-		humi += (int(data[14])) * 0.1
-		humi * -1
-	
-	if (data[20] is ' ') :
-		co2 += (int(data[21])) * 100
-	        co2 += (int(data[22])) * 10
-        	co2 += (int(data[23])) * 1		
-	else :
-		co2 += (int(data[20])) * 1000
-		co2 += (int(data[21])) * 100
-		co2 += (int(data[22])) * 10
-		co2 += (int(data[23])) * 1
+    data = ser.read(28)
+    print data
+    temp = 0
+    humi = 0
+    co2 = 0
+    #	print data[2], data[3], data[4], data[6]
+    #	print data[10], data[11], data[12], data[14]
+    #	print data[23]
+    if not (data[2].isdigit() & data[3].isdigit() & data[4].isdigit() & data[6].isdigit()) :
+        print 3
+        continue
+    if not (data[10].isdigit() & data[11].isdigit() & data[12].isdigit() & data[14].isdigit()) :
+        print 4
+        continue
+    if not (data[23].isdigit()) :
+        print 5
+        continue
+    #	print 2
+    if (data[1] is '+') :
+        temp += (int(data[2])) * 100
+        temp += (int(data[3])) * 10
+        temp += (int(data[4])) * 1
+        temp += (int(data[6])) * 0.1
+    else :
+        temp += (int(data[2])) * 100
+        temp += (int(data[3])) * 10
+        temp += (int(data[4])) * 1
+        temp += (int(data[6])) * 0.1
+        temp * -1
 
-	temp = temp-3.5
+    if (data[9] is '+') :
+        humi += (int(data[10])) * 100
+        humi += (int(data[11])) * 10
+        humi += (int(data[12])) * 1
+        humi += (int(data[14])) * 0.1
+    else :
+        humi += (int(data[10])) * 100
+        humi += (int(data[11])) * 10
+        humi += (int(data[12])) * 1
+        humi += (int(data[14])) * 0.1
+        humi * -1
+
+    if (data[20] is ' ') :
+        co2 += (int(data[21])) * 100
+        co2 += (int(data[22])) * 10
+        co2 += (int(data[23])) * 1
+    else :
+        co2 += (int(data[20])) * 1000
+        co2 += (int(data[21])) * 100
+        co2 += (int(data[22])) * 10
+        co2 += (int(data[23])) * 1
+
+    temp = temp-3.5
 #	print 1
-	print temp
-	print humi
-	print co2
-	print datetime.datetime.today().ctime()
-	
-	now = time.localtime()
-	sql = "insert into Pure_data (nodeid, Temperature, Humidity, Co2, regdate) values (1, %.1f, %.1f, %d, now()+ interval 9 hour )" % (temp, humi, co2)
-	cursor.execute(sql)
-	db.commit()
-	time.sleep(10)
+    print temp
+    print humi
+    print co2
+    print datetime.datetime.today().ctime()
+
+    now = time.localtime()
+    sql = "insert into Pure_data (nodeid, Temperature, Humidity, Co2, regdate) values (1, %.1f, %.1f, %d, now()+ interval 9 hour )" % (temp, humi, co2)
+    cursor.execute(sql)
+    db.commit()
+    time.sleep(10)
 
 ser.close()
